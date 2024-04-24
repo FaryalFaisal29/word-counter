bb1487e585ec784e5870a34f6284ae8e2ffa3b9f
FaryalFaisal29 <164797311+FaryalFaisal29@users.noreply.github.com>
2024-04-24 13:54:41 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+import chalk from "chalk"
+
+
+
+  let user_Answers = await inquirer.prompt
+     (
+       
+        [
+            {
+                  type: "input",
+                  name: "Sentence",
+               message: (chalk.redBright.bold("Please Enter Your Sentence To Count The Words:"))
+            },
+ 
+        ]
+    );
+  
+    const words = user_Answers.Sentence.trim().split(" ");
+  
+    // Print the array of words
+    console.log(words);
+    
+    // Print the word count of the sentence
+    console.log(chalk.bgCyan.bold(`Your Sentence word count is ${words.length}`));
